736a32f34ee8ef8712956204bc0f51bd96114068
unknown <albirosaldoalfredo12@gmail.com>
2022-11-07 10:09:31 +0200

    primeiro commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let fecha = document.querySelector(".baras")
+let menu = document.querySelector(".navlinks")
+fecha.onclick = () =>{
+    fecha.classList.toggle("close")
+    menu.classList.toggle("active")
+}
